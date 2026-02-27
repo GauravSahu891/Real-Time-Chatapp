0440b6ac916072c6d05329c5ab37444cb481a15a
GauravSahu891 <gauravsahu891@gmail.com>
2026-02-27 07:04:45 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -11,7 +12,6 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
